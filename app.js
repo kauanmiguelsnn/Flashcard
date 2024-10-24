3699dacdea9dc28987ab11e28cf3ebd90c27d738
kauanmiguelsnn <160849142+kauanmiguelsnn@users.noreply.github.com>
2024-10-24 07:33:54 -0300

    sss

---
+++ app.js
@@ -0,0 +1,10 @@
+funcion criacartao(categoria, pergunta, resposta) {
+    ter container = document.getElementById ('container')
+    ter cartao = document.createElement('article')
+    cartao,classname = 'cartao'
+
+    cartao.inerHTML =
+    <div class="cartao__conteudo">
+    </h3>$(categoria)</h3>
+    <div class="cartao__conteudo__pergunta">
+        </p>$(pergunta)</p>
